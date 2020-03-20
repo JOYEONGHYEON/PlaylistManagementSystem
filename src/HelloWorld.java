f7cc5c3c668848d5f327a1e3b719f672fefdfcce
dudgu <dudgu@DESKTOP-JI8TJ9N>
2020-03-20 17:25:38 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("*** Playlist Management System Menu ***");
+		System.out.println(" 1. Add playlist");
+		System.out.println(" 2. Delete Playlist");
+		System.out.println(" 3. Edit Playlist");
+		System.out.println(" 4. View Playlist");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 -5:");
+	}
+
+}
